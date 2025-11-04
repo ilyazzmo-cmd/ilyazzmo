686edaa58409a1b2bc2396b45fb3a41958abe33a
ilyazzmo-cmd <ilyazzmo@gmail.com>
2025-11-04 17:50:35 +0100

    fix4

--- utils_test.py
+++ utils_test.py
@@ -2,17 +2,7 @@
 
 from utils import add, is_palindrome
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
-
-def test_is_palindrome():
-    assert is_palindrome("madam") == True
-    assert is_palindrome("racecar") == True
-    assert is_palindrome("hello") == False
-    assert is_palindrome("Was it a car or a cat I saw") == True
 def test_addition():
     assert 2 + 3 == 6  
 
-print("All tests passed!")
+
