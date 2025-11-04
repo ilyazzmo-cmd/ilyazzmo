71677854dad01159457754675084393955d9985c
ilyazzmo-cmd <ilyazzmo@gmail.com>
2025-11-04 18:15:00 +0100

    fix6

--- utils_test.py
+++ utils_test.py
@@ -3,7 +3,7 @@ from utils import add, is_palindrome
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 1  
+    assert add(0, 0) == 0 
 def test_is_palindrome():
     assert is_palindrome("madam") == True
     assert is_palindrome("racecar") == True
